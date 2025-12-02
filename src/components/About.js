1e15cf86b1677b3832346ec042164ff1268f0f28
handesaurabh <handesaurabh2004@gmail.com>
2025-12-02 13:07:34 +0530

    Create About.js

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+    return (
+      <div>
+      About
+      <p>This is a sample React Router program.<p>
+      </div>
+    )
+}
+
+export default About
