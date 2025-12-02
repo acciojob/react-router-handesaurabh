0f83f89ef10fa1a18457a028a9bab7487b194381
handesaurabh <handesaurabh2004@gmail.com>
2025-12-02 13:12:02 +0530

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,12 @@
-import React from 'react'
+import React from "react";
 
-const About = () => {
-    return (
-      <div>
-      About
-      <p>This is a sample React Router program.<p>
-      </div>
-    )
+function About() {
+  return (
+    <div>
+      <h2>About</h2>
+      <p>This is a sample React Router program.</p>
+    </div>
+  );
 }
 
-export default About
+export default About;
